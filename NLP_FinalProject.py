98d31cd5ecb1e89aa01b6cd6ef797c8d3b75c2c2
ravitshagan <34209587+ravitshagan@users.noreply.github.com>
2025-02-08 22:23:48 +0200

    Update NLP_FinalProject.py

--- NLP_FinalProject.py
+++ NLP_FinalProject.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-get_ipython().system('pip install transformers torch tqdm scikit-learn pandas')
-
-
-# In[2]:
-
 
 # Import necessary libraries
 import torch
