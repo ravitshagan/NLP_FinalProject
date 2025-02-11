27c12ff3124cf98661e86bc7f586fa6c8bb65680
ravitshagan <34209587+ravitshagan@users.noreply.github.com>
2025-02-11 15:30:21 +0200

    Update NLP_FinalProject_distilgpt2_2.py

--- NLP_FinalProject_distilgpt2_2.py
+++ NLP_FinalProject_distilgpt2_2.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
-from transformers import DistilGPT2LMHeadModel, AutoTokenizer
+from transformers import GPT2LMHeadModel, AutoTokenizer
 from torch.optim import AdamW
 from datasets import load_dataset
 from sklearn.model_selection import train_test_split
